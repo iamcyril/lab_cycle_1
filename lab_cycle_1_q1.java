fe7bf86ef75365438238bc666d1c72f93330958d
iamcyril <156396721+iamcyril@users.noreply.github.com>
2026-06-29 20:50:36 +0530

    Add files via upload

---
+++ lab_cycle_1_q1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class lab_cycle_1_q1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Roll No: ");
+        int rollNo = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Course: ");
+        String course = sc.nextLine();
+
+        System.out.print("Percentage: ");
+        double percentage = sc.nextDouble();
+
+        System.out.println("\nStudent Details");
+        System.out.println("---------------");
+        System.out.println("Name       : " + name);
+        System.out.println("Roll No    : " + rollNo);
+        System.out.println("Course     : " + course);
+        System.out.println("Percentage : " + percentage);
+
+        sc.close();
+    }
+}

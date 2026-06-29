9bbdd48cf57fa243e2612f3ef61216006d38f032
iamcyril <156396721+iamcyril@users.noreply.github.com>
2026-06-29 21:58:32 +0530

    Add files via upload

---
+++ lab_cycle_1_q8.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class lab_cycle_1_q8 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number (A): ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number (B): ");
+        int b = sc.nextInt();
+
+        int addition = a + b;
+        int subtraction = a - b;
+        int multiplication = a * b;
+        int division = a / b;
+        int modulus = a % b;
+
+        System.out.println("Addition = " + addition);
+        System.out.println("Subtraction = " + subtraction);
+        System.out.println("Multiplication = " + multiplication);
+        System.out.println("Division = " + division);
+        System.out.println("Modulus = " + modulus);
+
+        sc.close();
+    }
+}
